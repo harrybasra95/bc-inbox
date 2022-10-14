b76f6688e75264fcb733eafd2c3497576eeeb1e6
Hanarinder Singh <harnarinder.singh@yum.com>
2022-10-14 10:57:53 +0530

    feat: initial commit Added boilerplate code for the project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+
+// Parsing config for requests
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// EJS files config
+app.set('views', './src/views')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(3001, () => {
+    console.log('listening on port 3000')
+})
